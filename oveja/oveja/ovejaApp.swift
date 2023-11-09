c035428e3837ede030c30f248b424ed1382fc5d0
navarrete2 <m4gu4l@icloud.com>
2023-11-09 17:08:03 +0100

    archivo inicial

---
+++ oveja/oveja/ovejaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ovejaApp.swift
+//  oveja
+//
+//  Created by Miguel  on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ovejaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
